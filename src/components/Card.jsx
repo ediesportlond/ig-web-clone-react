279635e10bf25ca0e913ac8fcf5365f0f12bf1f5
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-11-17 18:00:04 -0500

    added likes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,59 @@
-import { Card, Avatar } from 'antd'
+import { useState, useEffect } from 'react'
+import { Card, Avatar, Button } from 'antd'
 
 export default function Post({ photo }) {
+  const [isLiked, setIsLiked] = useState(false)
+  const [likes, setLikes] = useState(0)
+  const userId = 1
+  const like = () => {
+    fetch(`https://express-ts-ee.web.app/likes/${photo.id}/${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then((res) => {
+        setIsLiked(res.isLiked)
+        setLikes(res.likes)
+      })
+      .catch(console.error)
+  }
+
+  useEffect(() => {
+    fetch(`https://express-ts-ee.web.app/likes/${photo.id}/${userId}`)
+      .then(res => res.json())
+      .then((res) => {
+        setIsLiked(res.isLiked)
+        setLikes(res.likes)
+      })
+      .catch(console.error)
+  }, [setIsLiked, userId, photo.id])
+
   return (
     <>
-  <Card
-    style={{
-      width: 300,
-    }}
+      <Card
+        style={{
+          width: 300,
+        }}
 
-    hoverable
-    cover={
-      <img
-        alt={photo.description}
-        src={photo.photo}
-      />
-    }
-    // actions={[
-    //   <SettingOutlined key="setting" />,
-    //   <EditOutlined key="edit" />,
-    //   <EllipsisOutlined key="ellipsis" />,
-    // ]}
-  >
-    <Card.Meta
-      avatar={<Avatar src={photo.profilePic} />}
-      title={photo.username}
-      description={photo.description}
-    />
-  </Card>
-      {/* <img src={photo.photo} alt={photo.description} />
-      <img src={photo.profilePic} alt={photo.username} width="250" />
-      <p>{photo.username}</p>
-      <p>{photo.description}</p> */}
+        hoverable
+        cover={
+          <img
+            alt={photo.description}
+            src={photo.photo}
+          />
+        }
+        actions={[
+          <Button onClick={like}>{isLiked ? "💙" : "🤍"}</Button>
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar src={photo.profilePic} />}
+          title={"Likes " + likes}
+          description={photo.username + "=> " + photo.description}
+        />
+      </Card>
     </>
   )
 }
